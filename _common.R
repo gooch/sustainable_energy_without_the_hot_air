7444b9b8215b46715f094987b06b1f770d5fd37c
Ross Paine <ashtermet@gmail.com>
2017-05-13 14:53:49 +0800

    reduce the width of figures, and don't echo code snippets

--- _common.R
+++ _common.R
@@ -3,9 +3,10 @@ options(digits = 3)
 
 knitr::opts_chunk$set(
   comment = "#>",
+  echo = FALSE,
   collapse = TRUE,
   cache = TRUE,
-  out.width = "70%",
+  out.width = "30%",
   fig.align = 'center',
   fig.width = 6,
   fig.asp = 0.618,  # 1 / phi
